81cbcdc0f23e4062d5a08c322dd46f60f56c879d
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-06-20 00:16:04 +0900

    課題18-1,GETとPOSTを実装

--- src/main/java/raisetech/StudentManagementSystem/StudentManagementSystemApplication.java
+++ src/main/java/raisetech/StudentManagementSystem/StudentManagementSystemApplication.java
@@ -3,18 +3,37 @@ package raisetech.StudentManagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementSystemApplication {
 
+  private String name = "Tarou Yamada";
+  private String age = "25";
+
+
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementSystemApplication.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello,World!";
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
   }
+
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  //名前だけ変える
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
+  }
+
 }
