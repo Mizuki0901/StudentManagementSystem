6687c13efa795eb12fd98c8b050d52c83f9c9065
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-07-10 14:43:18 +0900

    READ処理のConvert部分の実装

--- src/main/java/raisetech/student/management/system/data/Student.java
+++ src/main/java/raisetech/student/management/system/data/Student.java
@@ -16,5 +16,6 @@ public class Student {
   private String gender;
   private String mailaddress;
   private String address;
-
+  private String remark;
+  private boolean isDeleted;
 }
--- src/main/java/raisetech/student/management/system/service/StudentService.java
+++ src/main/java/raisetech/student/management/system/service/StudentService.java
@@ -18,26 +18,12 @@ public class StudentService {
     this.repository = repository;
   }
 
-  /**
-   * 20代の受講生を抽出
-   *
-   * @return　20代の受講生の情報一覧
-   */
   public List<Student> searchStudentList() {
-    return repository.searchStudent().stream()
-        .filter(student -> student.getAge() >= 20 && student.getAge() < 30)
-        .collect(Collectors.toList());
+    return repository.searchStudent();
   }
 
-  /**
-   * Javaコースの情報を抽出
-   *
-   * @return　Javaコースの情報一覧
-   */
   public List<StudentCourse> searchCourseList() {
-    return repository.searchCourse().stream()
-        .filter(student -> student.getCourseName().equals("Java"))
-        .collect(Collectors.toList());
+    return repository.searchCourse();
   }
 
 }
