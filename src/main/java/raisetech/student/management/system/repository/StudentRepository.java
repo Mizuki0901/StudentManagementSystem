d90e3a74a3f20c4322ae78248a9104cc384c7095
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-08-06 00:16:56 +0900

    xmlの追加

--- src/main/java/raisetech/student/management/system/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/system/repository/StudentRepository.java
@@ -1,12 +1,8 @@
 package raisetech.student.management.system.repository;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import raisetech.student.management.system.data.Student;
 import raisetech.student.management.system.data.StudentCourse;
 
@@ -21,7 +17,6 @@ public interface StudentRepository {
    *
    * @return　現在在籍する受講生情報(一覧)
    */
-  @Select("SELECT * FROM students WHERE is_deleted = 0")
   List<Student> searchStudents();
 
   /**
@@ -29,14 +24,12 @@ public interface StudentRepository {
    *
    * @return　退会した受講生情報(一覧)
    */
-  @Select("SELECT * FROM students WHERE is_deleted = 1")
   List<Student> searchDeleteStudents();
 
   /**
    * @param studentId(受講生id)
    * @return 受講生情報(単一)
    */
-  @Select("SELECT * FROM students WHERE student_id = #{studentId}")
   Student searchStudentById(@Param("studentId") int studentId);
 
   /**
@@ -44,7 +37,6 @@ public interface StudentRepository {
    *
    * @return　受講生コース情報(全件)
    */
-  @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchCourses();
 
   /**
@@ -53,7 +45,6 @@ public interface StudentRepository {
    * @param studentId(受講生id)
    * @return 受講生idに紐づくコース情報
    */
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> searchCourseById(@Param("studentId") int studentId);
 
   /**
@@ -61,11 +52,6 @@ public interface StudentRepository {
    *
    * @param student
    */
-  @Insert(
-      "INSERT INTO students(fullname,furigana,nickname,age,gender,mailaddress,area,remark,is_deleted)"
-          + " VALUES( #{fullname}, #{furigana}, #{nickname}, #{age}, #{gender}, "
-          + "#{mailaddress}, #{area}, #{remark}, false)")
-  @Options(useGeneratedKeys = true, keyProperty = "studentId", keyColumn = "student_id")
   void insertStudent(Student student);
 
   /**
@@ -73,9 +59,6 @@ public interface StudentRepository {
    *
    * @param studentCourse
    */
-  @Insert("INSERT INTO students_courses(student_id,course_name,date_start)"
-      + " VALUES( #{studentId}, #{courseName}, #{dateStart})")
-  @Options(useGeneratedKeys = true, keyProperty = "courseId", keyColumn = "course_id")
   void insertCourse(StudentCourse studentCourse);
 
   /**
@@ -83,17 +66,11 @@ public interface StudentRepository {
    *
    * @param student
    */
-  @Update(
-      "UPDATE students SET fullname=#{fullname}, furigana=#{furigana}, nickname=#{nickname}, age=#{age}, mailaddress=#{mailaddress},"
-          + "area=#{area}, remark=#{remark}, is_deleted=#{isDeleted} WHERE student_id=#{studentId}")
   void updateStudent(Student student);
 
   /**
    * 受講生コース情報のコース名を更新します。
    */
-  @Update(
-      "UPDATE students_courses SET course_id=#{courseId}, course_name=#{courseName}, date_start=#{dateStart},"
-          + "date_finish=#{dateFinish} WHERE course_id=#{courseId}")
   void updateCourse(StudentCourse studentCourse);
 
 }
